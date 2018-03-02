08e3c035623b717e2c312f4841e3814b908d8f94
quan <lizhengquan@DESKTOP-RQTC1LS>
2018-03-02 17:09:49 +0800

    1

---
+++ AutoStatistics/AutoStatistics/CoinDao.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using SharpDapper;
+using SharpDapper.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoXX
+{
+    public class CoinDao
+    {
+        public CoinDao()
+        {
+            string connectionString = "server=localhost;port=3306;user id=root; password=lyx123456; database=studyplan; pooling=true; charset=utf8mb4";
+            var connection = new MySqlConnection(connectionString);
+            Database = new DapperConnection(connection);
+
+        }
+        protected IDapperConnection Database { get; private set; }
+
+        public SpotStatistics GetSpotStatistics(string statisticsTime)
+        {
+            var sql = $"select * from t_spot_statistics where StatisticsTime='{statisticsTime}'";
+            var r = Database.Query(sql).FirstOrDefault();
+            return r;
+        }
+
+        public void CreateSpotStatistics(SpotStatistics spotStatistics)
+        {
+            using (var tx = Database.BeginTransaction())
+            {
+                Database.Insert(spotStatistics);
+                tx.Commit();
+            }
+        }
+
+        public List<SpotRecord> ListAll()
+        {
+            string sql = "select * from t_spot_record";
+            var r = Database.Query<SpotRecord>(sql).ToList();
+            return r;
+        }
+    }
+
+    [Table("t_spot_statistics")]
+    public class SpotStatistics
+    {
+        public long Id { get; set; }
+        public decimal TLCount { get; set; }
+        public decimal TLAmount { get; set; }
+        public decimal TLNowAmount { get; set; }
+        public decimal TotalSy { get; set; }
+        public decimal TotalLoss { get; set; }
+        public decimal AllEarning { get; set; }
+        public string StatisticsTime { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+
+    public class SpotRecord
+    {
+        public long Id { get; set; }
+        public string Coin { get; set; }
+        public decimal Selltotalquantity { get; set; }
+        public decimal Selltradeprice { get; set; }
+        public decimal Buytotalquantity { get; set; }
+        public decimal Buytradeprice { get; set; }
+        public bool SellSuccess { get; set; }
+
+        //selltradeprice, selltotalquantity, buytradeprice,buytotalquantity, selltradeprice* selltotalquantity-buytradeprice* buytotalquantity
+    }
+}
---
+++ AutoStatistics/AutoStatistics/SpotStatisticsBiz.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoXX;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace AutoStatistics
+{
+    public class SpotStatisticsBiz
+    {
+        public static void Statistics()
+        {
+            while (true)
+            {
+                var dt = DateTime.Now.ToString("yyyyMMddHH");
+                var spotStatisticsItem = new CoinDao().GetSpotStatistics(dt);
+                if (spotStatisticsItem != null)
+                {
+                    Thread.Sleep(1000 * 60);
+
+                    Console.WriteLine("当前统计 ->  " + JsonConvert.SerializeObject(spotStatisticsItem));
+                    continue;
+                }
+
+                // 统计 TLCount TLAmount TLNowAmount TotalSy TotalLoss AllEarning StatisticsTime
+                var spotRecords = new CoinDao().ListAll();
+                Console.WriteLine($"总记录数{spotRecords}");
+                int tlCount = 0;
+                decimal tlAmount = 0;
+                decimal tlNowAmount = 0;
+                decimal totalSy = 0;
+
+                Dictionary<string, decimal> coinCount = new Dictionary<string, decimal>();
+
+                foreach (var spotRecord in spotRecords)
+                {
+                    if (!spotRecord.SellSuccess)
+                    {
+                        tlCount++;
+                        tlAmount += spotRecord.Buytotalquantity * spotRecord.Buytradeprice;
+                        if (coinCount.ContainsKey(spotRecord.Coin))
+                        {
+                            coinCount[spotRecord.Coin] += spotRecord.Buytotalquantity;
+                        }
+                        else
+                        {
+                            coinCount.Add(spotRecord.Coin, spotRecord.Buytotalquantity);
+                        }
+                    }
+                    else
+                    {
+                        totalSy += spotRecord.Selltotalquantity * spotRecord.Selltradeprice -
+                                   spotRecord.Buytotalquantity * spotRecord.Buytradeprice;
+                    }
+                }
+
+                foreach (var coin in coinCount.Keys)
+                {
+                    // 获取当前价位
+
+                }
+
+                decimal totalLoss = tlAmount - tlNowAmount;
+                decimal allEarning = totalSy - totalLoss;
+
+                new CoinDao().CreateSpotStatistics(new SpotStatistics()
+                {
+                    CreateTime = DateTime.Now,
+                    StatisticsTime = dt,
+
+                    TLCount = tlCount,
+                    TLAmount = tlAmount,
+                    TLNowAmount = tlNowAmount,
+                    TotalSy = totalSy,
+
+                    TotalLoss = totalLoss,
+                    AllEarning = allEarning
+                });
+            }
+        }
+    }
+}
